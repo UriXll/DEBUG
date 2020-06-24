96104e176cddbf02a9cf1d2101579d9781d3b0b5
root <809961739@qq.com>
2020-06-24 19:03:51 +0800

    finished debug1

---
+++ homework/1/7.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+#define EPSILON 1e-7
+
+double bisection(int p, int q, double (*func)(int, int, double));
+double f(int p, int q, double x);
+int main() {
+    int p;
+    int q;
+    scanf("%d%d", &p, &q);
+    printf("%.4f\n", bisection(p, q, f));
+    return 0;
+}
+
+double bisection(int p, int q, double (*func)(int, int, double)) {
+    double min,max,mid;
+    min=-20;
+    max=20;
+    mid=(min+max)/2;
+    while(fabs(f(p,q,mid))>=EPSILON){
+        if(f(p,q,min)*f(p,q,mid)<0){
+            max=mid;
+        }
+        else{
+            min=mid;
+        }
+        mid=(min+max)/2.0;
+    }
+    return mid;
+}
+
+double f(int p, int q, double x) {
+    return p * x + q;
+}
