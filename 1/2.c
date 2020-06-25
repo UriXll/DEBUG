72fd73e3cf25c7a2e21499edd959cf4fc648c522
UriXll <yu1191336704@gmail.com>
2020-06-25 11:17:59 +0800

    reslove two problem

--- 1/2.c
+++ 1/2.c
@@ -13,15 +13,15 @@ int main() {
 }
 
 double bisection(int p, int q, double (*func)(int, int, double)) {
-	double a = 20.0,b = -20.0;
+	double a = 20.0, b = -20.0;
     double m;
-    while(1){
+    while(1) {
         m = (a + b) / 2;
-        if(fabs(func(p,q,m)) < EPSILON){
+        if(fabs(func(p,q,m)) < EPSILON) {
             return m;
-        }else if(f(p,q,a) * f(p,q,m) < 0){
+        } else if(f(p,q,a) * f(p,q,m) < 0) {
             b = m;
-        }else{
+        } else {
             a = m;
         }
     }
