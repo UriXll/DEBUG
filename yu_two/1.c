72b33d0f3fb5942a14ab693fef8da1330ff8cd3c
UriXll <62315930+UriXll@users.noreply.github.com>
2020-06-26 20:10:08 +0800

    initial

--- yu_two/1.c
+++ yu_two/1.c
@@ -1,35 +1,41 @@
 #include <stdio.h>
+#include<stdlib.h>
 
 int main() {
     int matrix_a[10][10];
     int matrix_b[10][10];
-    int m;
-    int n;
-	scanf("%d%d", &m, &n);
-    int i;
-    int j;
+    int m, n;
+    scanf("%d%d", &m, &n);
+
+    int i, j;
     for (i = 0; i < m; i++) {
         for (j = 0; j < n; j++) {
             scanf("%d", &matrix_a[i][j]);
         }
     }
-	for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) {
         for (j = 0; j < m; j++) {
             scanf("%d", &matrix_b[i][j]);
         }
     }
-    int sum[m][m];
+
+    int** sum = (int**)malloc(sizeof(int*) * m);//Initialize the result array
     for (i = 0; i < m; i++) {
-        for (j = 0; j < m; j++) {
-            sum[m][m] = 0;
-        }
+        sum[i] = (int*)malloc(sizeof(int) * m);
     }
     for (i = 0; i < m; i++) {
+        for (j = 0; j < m; j++) {
+            sum[i][j] = 0;
+        }
+    }
+
+    for (i = 0; i < m; i++) {//Start multiplication
         for (j = 0; j < m; j++) {
             for (int digit = 0; digit < n; digit++)
-            sum[i][j] = sum[i][j] + matrix_a[i][digit] * matrix_b[digit][j];
+                sum[i][j] = sum[i][j] + matrix_a[i][digit] * matrix_b[digit][j];
         }
     }
+
     for (i = 0; i < m; i++) {
         for (j = 0; j < m; j++) {
             printf("%d", sum[i][j]);
