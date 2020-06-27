7bcbac9747021667834ca80e23859703c60f032a
UriXll <yu1191336704@gmail.com>
2020-06-27 19:31:42 +0800

    Three

--- yu_three/1.c
+++ yu_three/1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#pragma warning(disable:4996)
 
 int main() {
     int matrix[100][100];
@@ -15,7 +16,12 @@ int main() {
     int temp;
     while (m - x > 1 && n - x > 1) {
         for (temp = 0; temp < n - 1 - x; temp++) {
-            printf("%d ", matrix[x][temp + x]);
+            if (x == m / 2 && (temp + x) == n / 2) {
+                printf("%d ", matrix[x][temp + x]);
+                return 0;
+            } else {
+                printf("%d ", matrix[x][temp + x]);
+              }
         }
         for (temp = 0; temp < m - 1 - x; temp++) {
             printf("%d ", matrix[temp + x][n - 1 - x]);
--- yu_three/2.c
+++ yu_three/2.c
@@ -2,64 +2,74 @@
 
 int main() {
     int matrix[100][100];
-    int m;
-    int n;
+    int m, n;
     scanf("%d %d", &m, &n);
     int k = m * n;
     int m0 = m;
     int n0 = n;
-	for(int i = 0; i < m; i++) {
-        for(int j = 0; j < n; j++) {
-            scanf("%d ", &matrix[i][j]);
+    int i, j;
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
         }
     }
-    if(m == 1 || n == 1) {
-        for(int i = 0; i < m; i++) {
-        	for(int j = 0; j < n; j++) {
-				printf("%d", matrix[i][j]);
-        		if(j != n - 1) printf(" ");
-        	}
-            if(i != m - 1) printf(" ");
-    	}
-        return;
+    /*-------------------------------------------*/
+    if (m == 1 || n == 1) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                printf("%d", matrix[i][j]);
+                if (j != n - 1) printf(" ");
+            }
+            if (i != m - 1) printf(" ");
+        }
+        return 0;
     }
-    int i = 0;
-    while(i != k) {
-		for(int j = n - n0; j < n0 - 1; j++) {
+    /*-------------------------------------------*/
+    i = 0;
+    while (i != k) {
+        if ((n - n0 == n / 2) && (m - m0 == m / 2)) {
+            printf("%d", matrix[m / 2][m / 2]);
+            return 0;
+        }
+        for (j = n - n0; j < n0 - 1; j++) {
             printf("%d", matrix[m - m0][j]);
             i++;
-            if(i == k)
+            if (i == k) {
                 break;
-            else
+            } else {
                 printf(" ");
+              }
         }
-		for(int j = m - m0; j < m0 - 1; j++) {
+        for (j = m - m0; j < m0 - 1; j++) {
             printf("%d", matrix[j][n0 - 1]);
             i++;
-			if(i == k)
+            if (i == k) {
                 break;
-            else
+            } else {
                 printf(" ");
+              }
         }
-		for(int j = n0 - 1; j > n - n0; j--) {
+        for (j = n0 - 1; j > n - n0; j--) {
             printf("%d", matrix[m0 - 1][j]);
             i++;
-            if(i == k)
+            if (i == k) {
                 break;
-            else
+            } else {
                 printf(" ");
+              }
         }
-		for(int j = m0 - 1; j > m - m0; j--) {
+        for (j = m0 - 1; j > m - m0; j--) {
             printf("%d", matrix[j][n - n0]);
             i++;
-            if(i == k)
+            if (i == k) {
                 break;
-            else
+            } else {
                 printf(" ");
+              }
         }
         n0--;
         m0--;
     }
-    
+
     return 0;
 }
