106bbd5a20890148c6457fa4181210b78651404d
Your Name <you@example.com>
2020-06-28 22:45:33 +0800

    finished debug chenxu6

---
+++ chenxu6/7.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+
+void add(char);
+void delete (char);
+int rwx = 0;
+
+int main()
+{
+    //initiation
+    char temp;
+    char symbol;
+    scanf("%c", &temp);
+    if (temp == 'x') {
+        rwx ^= (1 << 0);
+    } else if (temp == 'w') {
+          rwx ^= (1 << 1);
+          scanf("%c", &temp);
+          if (temp == 'x') {
+              rwx ^= (1 << 0);
+          }
+      } else if (temp == 'r') {
+            rwx ^= (1 << 2);
+            scanf("%c", &temp);
+            if (temp == 'w') {
+                rwx ^= (1 << 1);
+                scanf("%c", &temp);
+                if (temp == 'x') {
+                    rwx ^= (1 << 0);
+                }
+            }
+        }  
+    //modification
+    while (scanf("%c", &symbol) != EOF) {
+        if (symbol == '+') {
+            scanf("%c", &temp);
+            add(temp);
+        } else if (symbol == '-') {
+              scanf("%c", &temp);
+              delete (temp);
+          }
+    }
+    //output
+    printf("%d", rwx);
+
+    return 0;
+} 
+
+void add(char input)
+{
+    if (input == 'r' && rwx / 4 != 1) {
+        rwx ^= (1 << 2);
+    } else if (input == 'w' && (rwx / 2 % 2 != 1)) {
+          rwx ^= (1 << 1);
+      } else if (rwx % 4 != 1) {
+            rwx ^= (1 << 0);
+        }
+}
+
+void delete (char input)
+{
+    if (input == 'r' && rwx / 4 == 1) {
+        rwx ^= (1 << 2);
+    } else if (input == 'w' && (rwx / 2 % 2 == 1)) {
+        rwx ^= (1 << 1);
+      } else if (rwx % 4 == 1) {
+            rwx ^= (1 << 0);
+        }
+}
