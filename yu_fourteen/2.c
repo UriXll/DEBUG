7fc22ca3929a09afb4e713e574a0ce7f038e3321
UriXll <62315930+UriXll@users.noreply.github.com>
2020-06-30 21:28:23 +0800

    debug has defeated

---
+++ yu_fourteen/2.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int n;
+    int i, j;
+    scanf("%d", &n);
+    for (i = n; i >= 1; i--) {
+        for (j = i; j >= 1; j--) {
+            printf("%d ", j);
+            if (j == 1) {
+                printf("\n");
+            }
+        }
+    }
+    return 0;
+}
