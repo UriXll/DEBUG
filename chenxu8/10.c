471b398619c3d5074e3cad53f240a5ca973efd7c
xihuanyintian <66348361+xihuanyintian@users.noreply.github.com>
2020-06-30 17:56:37 +0800

    Update 10.c

--- chenxu8/10.c
+++ chenxu8/10.c
@@ -1,33 +1,32 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
+int main()
+{
     int n;
     scanf("%d\n", &n);
     int i, j, k;
-    char a[100][100] = {'0'};
-    char temp[100] = {'0'};
+    char a[100][101]={'0'};
+    char temp[101] = {'0'};
 
     for (i = 0; i < n; i++) {
-        for (j = 0; j < 100; j++) {
-            if (scanf("%c", &a[i][j]) == EOF) {
-                break;
-            }
+        for (j = 0; j < 101; j++) {
+            scanf("%c", &a[i][j]);
             if (a[i][j] == '\n') {
+                a[i][j] = '\0';
                 break;
             }
         }
     }
-
+    
     for (i = 0, k = 0; i < n - 1; i++) {
-        if (strlen(a[k]) > strlen(a[i + 1])) {
+        if (strlen(a[k]) >= strlen(a[i + 1])) {
             strcpy(temp, a[k]);
         } else {
-              k = i + 1;
-              strcpy(temp, a[k]);
-          }
+            k = i + 1;
+            strcpy(temp, a[k]);
+        }
     }
-    
     printf("%s", temp);
 
     return 0;
