2352e313220504174ad0b534a8d308cb5a5a7b89
UriXll <62315930+UriXll@users.noreply.github.com>
2020-06-30 16:05:00 +0800

    first version

--- yu_nine/1.c
+++ yu_nine/1.c
@@ -1,16 +1,19 @@
-#include <stdio.h>
-int cal_step(int a[], int n){
+ï»¿#include <stdio.h>
+
+int cal_step(int a[], int n)
+{
     return a[n] = a[n - 2] + a[n - 3];
 }
-int main() {
-	int N;//×ÜÌ¨½×Êı
+int main()
+{
+    int N;//æ€»å°é˜¶æ•°
     scanf("%d", &N);
-    int step[50];//step[N]N¼¶Ì¨½×µÄ·½·¨Êı
+    int step[50];//step[N]Nçº§å°é˜¶çš„æ–¹æ³•æ•°
     step[0] = 0;
     step[1] = 0;
     step[2] = 1;
     step[3] = 1;
-    for (int i = 4; i <= N; i++){
+    for (int i = 4; i <= N; i++) {
         step[i] = step[i - 2] + step[i - 3];
     }
     printf("%d", step[N]);
