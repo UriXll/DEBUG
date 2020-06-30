247a7c6733cb26c31fde67f28c919f0d25722a18
UriXll <yu1191336704@gmail.com>
2020-06-30 04:18:03 -0400

    Finished

--- yu_nine/4.c
+++ yu_nine/4.c
@@ -8,8 +8,9 @@ int main()
     arr[0] = 0;
     arr[1] = 0;
     arr[2] = 1;
+    arr[3] = 1;
 
-    for (i = 3; i <= n; i++) {
+    for (i = 4; i <= n; i++) {
         arr[i] = arr[i - 2] + arr[i - 3];
     }
     printf("%d", arr[n]);
--- yu_nine/5.c
+++ yu_nine/5.c
@@ -1,17 +1,20 @@
 ﻿#include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
     int N;
     int i;
-    int part[N];
+    scanf("%d", &N);
+    int* part = (int*)malloc(sizeof(int) * N);
+    part[1] = 0;
     part[2] = 1;
     part[3] = 1;
     part[4] = 1;
-    scanf("%d", &N);
     for (i = 5; i <= N; i++) {
         part[i] = part[i - 2] + part[i - 3];
     }
     printf("%d", part[N]);
     return 0;
 }
+
