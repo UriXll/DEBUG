f9edc696c27004f32d9cfb3776b2278f106b865b
UriXll <62315930+UriXll@users.noreply.github.com>
2020-06-30 21:05:01 +0800

    so easy

---
+++ yu_twelve/1.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <math.h>
+#define PI 3.14159
+
+int main()
+{
+    double r;
+    double a, b;
+    scanf("%lf", &r);;
+    a = (double)2 * PI * r;
+    printf("%.3f\n", a);
+    b = (double)PI * r * r;
+    printf("%.3f", b);
+    return 0;
+}
