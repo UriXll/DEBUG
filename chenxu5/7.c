1f7b78739c326e1e28f4e2dd352d90cf4fd30d47
xihuanyintian <66348361+xihuanyintian@users.noreply.github.com>
2020-06-30 16:28:45 +0800

    Update 7.c

--- chenxu5/7.c
+++ chenxu5/7.c
@@ -1,36 +1,38 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-void swap(char **b, char **c)
+void swap (char *b, char *c)
 {
-    char *temp = (char *)malloc(31);
-    temp = *b;
-    *b = *c;
-    *c = temp;
+    char *temp=(char*)malloc(21);
+    strcpy(temp, b);
+    strcpy(b, c);
+    strcpy(c, temp);
 }
 
 int main()
 {
-    char name[20][31];
+    char name[10][21];
     int i;
     int j;
     int m = 10;
 
     for (i = 0; i < m; i++) {
-        scanf("%s", name[i]);
+        scanf("%s", name[i]);    
     }
-
     for (j = 0; j < m; j++) {
-        for (i = j + 1; i < m; i++) {
-            if (strcmp(name[j], name[i]) > 0) {
-                swap(&name[j], &name[i]);
+        for (i = 0; i < m - j - 1; i ++) {
+            if (strcmp(name[i], name[i + 1]) > 0) { 
+                swap(name[i], name[i + 1]);    
             }
         }
     }
-
     for (i = 0; i < m; i++) {
-        printf("%s\n", name[i]);
+        if (i < 9) {
+            printf("%s\n", name[i]);
+        } else {
+            printf("%s", name[i]);
+        }
     }
-    
     return 0;
 }
