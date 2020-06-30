1c7d93f7b589d7cad6d10bff71c013f0a9705924
xihuanyintian <66348361+xihuanyintian@users.noreply.github.com>
2020-06-30 17:50:35 +0800

    Update 7.c

--- chenxu8/7.c
+++ chenxu8/7.c
@@ -6,28 +6,33 @@ int main()
     int n;
     int i, j;
     scanf("%d", &n);
-    char names[100][101];
+    char names[n][101];
     char temp[101];
 
-    for (i = 0; i < n + 1; i++) {
-        for (j = 0; j < 101; j++) {
-            scanf("%c", &names[i][j]);
-            if (names[i][j] == '\n') {
-                break;
+    i = 0;
+    j = 0;
+    while (scanf("%c", &names[i][j]) != EOF) {
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < 101; j++) {
+                scanf("%c", &names[i][j]);
+                if (names[i][j] == '\n') {
+                    names[i][j] = '\0';
+                    break;
+                }     
             }
         }
     }
 
-    for (i = 1; i < n + 1; i++) {
-        for (j = 1; j <= n - i; j++) {
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n - 1 - i; j++) {
             if (strlen(names[j]) < strlen(names[j + 1])) {
                 strcpy(temp, names[j]);
                 strcpy(names[j], names[j + 1]);
-                strcpy(names[j + 1], temp);
-            }
-        }
-    }
-    printf("%s\n", names[1]);
+                strcpy(names[j + 1], temp);               
+            }            
+        }   
+    } 
     
+    printf("%s", names[0]);   
     return 0;
 }
