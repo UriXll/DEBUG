a3bf94aa6dc6f6d147b1c92407aa14c87b95a74f
UriXll <62315930+UriXll@users.noreply.github.com>
2020-06-30 19:39:50 +0800

    Finished

--- yu_ten/1.c
+++ yu_ten/1.c
@@ -1,19 +1,25 @@
-#include <stdio.h>
-int main() {
-    int a,b,c,d;
-    scanf("%d",&a);
-    for(d=1;d<=a;d+=1){
-        for(b=1;b<d;b+=1){
-            c=0;
-            if(d%b==0){
-             c+=1;
+﻿#include <stdio.h>
+
+int main()
+{
+    int a, b, c, d;
+    scanf("%d", &a);
+    if (a >= 2) {
+        printf("2\n");
+    }
+    for (d = 3; d <= a; d += 1) {
+        for (b = 2; b < d; b += 1) {
+            c = 0;
+            if (d % b == 0) {
+                c += 1;
+                break;
             }
         }
-        if(c==0){
-            printf("%d\n",d);
+        if (c == 1) {
+            continue;
         }
+        printf("%d\n", d);
     }
-   
-        
+
     return 0;
 }
--- yu_ten/2.c
+++ yu_ten/2.c
@@ -1,30 +1,21 @@
-#include<stdio.h>
+﻿#include<stdio.h>
+
 int main()
 {
-   int n,i,j;
-    scanf("%d",&n);
-   if(n==2)
-   {
-   printf("2\n");
-   }
-    else
-        printf("2\n");
-    {
-        for(i=3;i<=n;i=i+1)
-        {
-            for(j=2;j<=i;j=j+1)
-            {if(i%j==0)
-                {
+    int n, i, j;
+    scanf("%d", &n);
+    printf("2\n");
+    for (i = 3; i <= n; i = i + 1) {
+        for (j = 2; j < i; j = j + 1) {
+            if (i % j == 0) {
+                break;
+            } else {
+                if (j == i - 1) {
+                    printf("%d\n", i);
                     break;
                 }
-            else
-            {
-                printf("%d\n",i);
-                break;
-            }
             }
         }
     }
     return 0;
 }
-	
